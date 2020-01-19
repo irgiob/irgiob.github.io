cb1edf949b85ddf2e0df8a1305e6a17c2b799835
irgiob <irgiob@gmail.com>
2020-01-19 18:50:26 +0700

    added first version of website

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # irgiob.github.io
-Portfolio website to show all past and current coding projects by me
+
+This is a portfolio website I made to show the projects I've done here on Github
+in a more visual medium.
+
+Although the website was built off a template by TEMPLATED, extensive modifications
+have been made to make the website better suit my personal tastes.
+
+## Changes made
+* Added typing animation to main title
+* Switched the places of section two and three
+* Completely changed section three from an image gallery to resume information section
+* Added animations to section two that make the images change and become bigger
+* Removed nav bar
+* Added skills bars to section three
+* Extensive changes to CSS
+* Added a Dark Mode toggle
+
+If you would like to see the differences between the original template and my version,
+you can see the original template [here](https://templated.co/spatial)
---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+/*
+	Spatial by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+	});
+
+})(jQuery);
